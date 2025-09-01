da997a72419cee9bacdd6be0f6983d5d45dbe511
MindsMap-intern <stdk87267@gmail.com>
2025-09-01 23:39:51 +0530

    procfile update

--- weather_server.py
+++ weather_server.py
@@ -91,6 +91,6 @@ Forecast: {period['detailedForecast']}
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8000))  # Railway provides PORT env
+    port = int(os.environ.get("PORT", 8080))  # Railway provides PORT env
     mcp.run(transport="streamable-http", host="0.0.0.0", port=port)
 
